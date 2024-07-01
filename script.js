cd3f2e7f5ed8d68d20741578988defcda27aa159
dev-nasir <nasirconceptrecall@gmail.com>
2024-07-01 11:53:38 -0700

    patatice add and delete

---
+++ script.js
@@ -0,0 +1,19 @@
+let inputs = document.getElementById("inp");
+let text = document.querySelector(".text");
+
+function Add() {
+    if (inputs.value == "") {
+        alert("Please Enter task")
+    }
+
+    else {
+        let newEle = document.createElement("ul");
+        newEle.innerHTML = `${inputs.value}<i class="fa fa-trash" aria-hidden="true"></i>`;
+        text.appendChild(newEle);
+        inputs.value = "";
+        newEle.querySelector("i").addEventListener("click", remove);
+        function remove() {
+            newEle.remove()
+        }
+    }
+}
